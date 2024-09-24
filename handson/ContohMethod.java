f80bbab85198697c1d40e5f6355717d391870366
Timot <2382010@unai.edu>
2024-09-24 10:16:59 +0700

    handson4

---
+++ handson/ContohMethod.java
@@ -0,0 +1,14 @@
+package handson;
+
+public class ContohMethod {
+    public static int hitungLuasPersegi(int sisi){
+        int luas = sisi * sisi;
+        return luas;
+    }
+
+    public static void main(String[] args) {
+        int sisi = 5;
+        int luas = hitungLuasPersegi(sisi);
+        System.out.println(" Luas persegi dengan sisi " + sisi + " adalah " + luas);
+    }
+}
